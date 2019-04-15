61857075d081d0e5d7c5536fb65587fede027007
smitchell1980 <smm9@pdx.edu>
2019-04-15 01:41:23 -0700

    Update to gen.py
    Added functions to generate the string attributes.  Also edited the open command to set the newline operator to blank.  This will prevent from extra newlines being added in windows environments

--- venv/gen.py
+++ venv/gen.py
@@ -1,7 +1,28 @@
 import csv
 import random
 
-with open('test.csv', mode='w') as employee_file:
+def convert(unique):
+    result = list("AAAAAAAxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    i = 6
+    cnt = 0
+    while unique > 0:
+        rem = unique % 26
+        result[i]=chr(ord('A')+rem)
+        unique = int(unique/26)
+        i = i-1
+        cnt = cnt +1
+    return "".join(result)
+
+def str4Select(index):
+    switcher = {
+        0: "AAAAxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+        1: "HHHHxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+        2: "OOOOxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+        3: "VVVVxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+        }
+    return switcher.get(index%4)
+
+with open('test.csv', mode='w' newline='') as employee_file:
     tupleCount = 1000000
     employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     numList = random.sample(range(0,tupleCount),tupleCount)
@@ -19,8 +40,8 @@ with open('test.csv', mode='w') as employee_file:
         unique3 = unique1
         evenOnePercent = onePercent *2
         oddOnePercent = onePercent * 2 + 1
-        line = '"'+str(unique1)+'","'+str(unique2)+'","'+str(two)+'","'+str(four)+'","'+str(ten)
-        line += '","' +str(twenty)+'","'+str(onePercent)+'","'+str(tenPercent)+'","'+str(twentyPercent)
-        line += '","'+str(fiftyPercent)+'","'+str(unique3)+'","'+str(evenOnePercent)+'","'+str(oddOnePercent)+'"'
+        #line = '"'+str(unique1)+'","'+str(unique2)+'","'+str(two)+'","'+str(four)+'","'+str(ten)
+        #line += '","' +str(twenty)+'","'+str(onePercent)+'","'+str(tenPercent)+'","'+str(twentyPercent)
+        #line += '","'+str(fiftyPercent)+'","'+str(unique3)+'","'+str(evenOnePercent)+'","'+str(oddOnePercent)+'"'
         #print(line)
-        employee_writer.writerow([unique1])
+        employee_writer.writerow([unique1, unique2, two, four, ten, twenty, onePercent, tenPercent, twentyPercent, fiftyPercent, unique3, evenOnePercent, oddOnePercent, stringu1, stringu2, string4])
